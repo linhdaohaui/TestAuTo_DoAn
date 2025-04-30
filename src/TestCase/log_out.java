b047445cad18a05120942d5fa34bd244980dac36
Linh Dao <daothuylinh6823@gmail.com>
2025-05-01 01:31:58 +0700

    them maven

--- src/TestCase/log_out.java
+++ src/TestCase/log_out.java
@@ -1,28 +1,26 @@
 package TestCase;
 
 import org.openqa.selenium.WebDriver;
+
+
+import Common.CommonFunction;
+import Common.Constant;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import Common.CommonFunction;
-import Common.Constant;
-
 public class log_out {
 	
 private static WebDriver driver;
-	
 	@BeforeTest
 	public void setUp() throws Exception {
 		driver = CommonFunction.initWebDriver(Constant.URL_TEST);
 		Interface.InterfaceLogin.login(driver, Constant.email, Constant.password, "");
 	}
-			
 	@AfterTest
 	public void tearDown() throws Exception {
 		CommonFunction.shutDownDriver(driver);
 	}
-	
 	@Test
 	public void Test_01() {
 		
